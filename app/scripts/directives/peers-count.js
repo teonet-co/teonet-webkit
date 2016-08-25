8f01e50e550915ed1ad37ee4ee845365e0c96100
Kirill Scherba <kirill@scherba.ru>
2016-08-25 17:15:15 +0300

    application GUI redesigned

--- app/scripts/directives/peers-count.js
+++ app/scripts/directives/peers-count.js
@@ -12,7 +12,7 @@ angular.module('teonetWebkitApp')
       
       restrict: 'AE',
       replace: 'true',
-      template: '<span class="label label-info label-peers-count">{{ peersInfo.count }}</span>',      
+      template: '<span class="label label-primary label-peers-count">{{ peersInfo.count }}</span>',      
       controller: ['$scope', 'teonet', function ($scope, teonet) {
           // This is Teonet based controller, exit if teonet undefined
           if(!teonet || teonet.notLoaded) { return; }
